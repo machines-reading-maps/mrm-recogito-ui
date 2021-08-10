c7a918ce028a693d855857c80ecced72414778f4
Rainer Simon <aboutgeo@gmail.com>
2021-08-10 11:38:12 +0200

    Add classify widget multiple choice rather than radio button

--- src/widgets/ClassifyWidget.jsx
+++ src/widgets/ClassifyWidget.jsx
@@ -3,37 +3,50 @@ import { BiHomeAlt, BiMessageSquareDetail, BiTargetLock } from 'react-icons/bi';
 
 import './ClassifyWidget.scss';
 
-export const ClassifyFormatter = annotation =>
-  annotation?.bodies.find(b => b.purpose === 'classifying')?.value;
+export const ClassifyFormatter = annotation => {
+  const classes = annotation ?  
+    annotation.bodies.filter(b => b.purpose === 'classifying') : [];
+
+  return classes.length === 1 ? classes[0].value : 'multi-class';
+}
 
 const ClassifyWidget = props => {
 
-  const current =
-    props.annotation?.bodies.find(b => b.purpose === 'classifying')?.value;
+  const currentClasses = props.annotation ? 
+    new Set(props.annotation.bodies.filter(b => b.purpose === 'classifying').map(b => b.value)) : new Set();
 
-  const onClick = value => () =>
-    props.onUpsertBody({ 
-      type: 'TextualBody',
-      value, 
-      purpose: 'classifying' 
-    });
+  const onClick = classToToggle => () => {
+
+    if (currentClasses.has(classToToggle)) {
+      const toRemove = props.annotation.bodies.find(b => 
+        b.purpose === 'classifying' && b.value === classToToggle);
+
+      props.onRemoveBody(toRemove);
+    } else {
+      props.onAppendBody({ 
+        type: 'TextualBody',
+        value: classToToggle, 
+        purpose: 'classifying' 
+      });
+    }
+  }
 
   return (
     <div className="r6o-widget mrm-classify-widget r6o-nodrag">
       <button
-        className={current === 'ENTITY' ? 'entity selected' : 'entity'}
+        className={currentClasses.has('ENTITY') ? 'entity selected' : 'entity'}
         onClick={onClick('ENTITY')}> 
         <BiHomeAlt /> <span>Entity</span>
       </button>
       
       <button 
-        className={current === 'LABEL' ? 'label selected' : 'label'}
+        className={currentClasses.has('LABEL') ? 'label selected' : 'label'}
         onClick={onClick('LABEL')}>
         <BiMessageSquareDetail /> <span>Label</span>
       </button>
       
       <button 
-        className={current === 'SYMBOL' ? 'symbol selected' : 'symbol'}
+        className={currentClasses.has('SYMBOL') ? 'symbol selected' : 'symbol'}
         onClick={onClick('SYMBOL')}>
         <BiTargetLock /> <span>Symbol</span>
       </button>
