eb0c23044181e8439f21b58adc94ff57bb866c80
Rainer Simon <aboutgeo@gmail.com>
2021-08-11 14:23:31 +0200

    Added support for WMTS source

--- src/TileSources.js
+++ src/TileSources.js
@@ -1,53 +1,67 @@
-const initUploadSource = response => response.text()
-  .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-  .then(data => {
-    const props = data.firstChild;
-    const width = parseInt(props.getAttribute('WIDTH'));
-    const height = parseInt(props.getAttribute('HEIGHT'));
+import OpenSeadragonWMTS from 'openseadragon-wmts';
 
-    // Initialize OpenSeadragon viewer
-    const viewer = OpenSeadragon({
-      id:'image-pane',
-      prefixUrl: "https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",
-      tileSources: [{
-        type: 'zoomifytileservice',
-        width: width,
-        height: height,
-        tilesUrl: `/document/${config.documentId}/part/${config.partSequenceNo}/tiles/`
-      }]
+const fetchUploadSource = config =>
+  fetch(`/document/${config.documentId}/part/${config.partSequenceNo}/manifest`)
+    .then(response => response.text())
+    .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+    .then(data => {
+      const props = data.firstChild;
+      const width = parseInt(props.getAttribute('WIDTH'));
+      const height = parseInt(props.getAttribute('HEIGHT'));
+
+      // Initialize OpenSeadragon viewer
+      const viewer = OpenSeadragon({
+        id:'image-pane',
+        prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/',
+        tileSources: [{
+          type: 'zoomifytileservice',
+          width: width,
+          height: height,
+          tilesUrl: `/document/${config.documentId}/part/${config.partSequenceNo}/tiles/`
+        }]
+      });
+
+      return { viewer };
     });
 
-    return { viewer };
-  });
-
-const initIIIFSource = response => response.json()
-  .then(manifest => {
-    // Initialize OpenSeadragon viewer
-    const viewer = OpenSeadragon({
-      id:'image-pane',
-      prefixUrl: "https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/",
-      tileSources: [ manifest ]
+const fetchIIIFSource = config =>
+  fetch(`/document/${config.documentId}/part/${config.partSequenceNo}/manifest`)
+    .then(response => response.json())
+    .then(manifest => {
+      // Initialize OpenSeadragon viewer
+      const viewer = OpenSeadragon({
+        id:'image-pane',
+        prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/',
+        tileSources: [ manifest ]
+      });
+      
+      return { viewer };
     });
-    
-    return { viewer };
+
+const fetchWMTSSource = config => {
+  const viewer = OpenSeadragon({
+    id: 'image-pane',
+    prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/'
   });
-
-const initWMTSSource = config => {
-
+  
+  return OpenSeadragonWMTS(viewer, {
+    url: `/document/${config.documentId}/part/${config.partSequenceNo}/manifest`
+  }).then(map => {
+    return { viewer, map };
+  });
 }
 
-export const initViewer = config =>
-  fetch(`/document/${config.documentId}/part/${config.partSequenceNo}/manifest`)
-    .then(response => {
-      const { contentType } = config;
+export const initViewer = config => {
 
-      if (contentType === 'IMAGE_UPLOAD') {
-        return initUploadSource(response);
-      } else if (contentType === 'IMAGE_IIIF') {
-        return initIIIFSource(response);
-      } else if (contentType === 'MAP_WMTS') {
-        return initWMTSSource(response);
-      }
-      
-      throw "Unsupported content type: " + contentType;
-    });
+  const { contentType } = config;
+
+  if (contentType === 'IMAGE_UPLOAD') {
+    return fetchUploadSource(config);
+  } else if (contentType === 'IMAGE_IIIF') {
+    return fetchIIIFSource(config);
+  } else if (contentType === 'MAP_WMTS') {
+    return fetchWMTSSource(config);
+  }
+  
+  throw "Unsupported content type: " + contentType;
+}
