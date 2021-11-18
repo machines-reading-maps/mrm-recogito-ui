5070abc000004b2dd4dc262fe29bbb4a2362d107
Rainer Simon <aboutgeo@gmail.com>
2021-11-18 14:24:35 +0100

    Entity API now points to HTTPS no5.at domain

--- src/widgets/EntityAPIConnector.js
+++ src/widgets/EntityAPIConnector.js
@@ -1,5 +1,5 @@
 const EntityAPIConnector = query =>
-  fetch(`http://51.105.40.135/entities?input=${query}`)
+  fetch(`https://mrm-entities.no5.at/entities?input=${query}`)
     .then(response => response.json())
     .then(data =>
       data.candidates.map(obj => ({ label: obj.entity, uri: obj.uri })));
