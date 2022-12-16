e5cb7d4f5fcf40b79538c4c14da7d6f60a5bbfb3
Rainer Simon <aboutgeo@gmail.com>
2022-12-16 18:25:45 +0100

    Added keyboard support to search dropdown

--- src/search/SearchWidget.jsx
+++ src/search/SearchWidget.jsx
@@ -4,12 +4,14 @@ import './SearchWidget.scss';
 
 const SearchResult = props => {
 
-  const { annotation, matches, onSelect } = props;
+  const { active, annotation, matches, onSelect } = props;
   
   const snippets = matches.map(match => match.value);
 
   return (
-    <li onClick={() => onSelect(annotation)}>
+    <li 
+      className={active ? 'active' : null}
+      onClick={() => onSelect(annotation)}>
       {snippets.join(' ... ')}
     </li>
   )
@@ -26,9 +28,16 @@ export const SearchWidget = props => {
 
   const [results, setResults] = useState([]);
 
+  const [active, setActive] = useState(null);
+
   useEffect(() => {
-    if (open)
+    if (open) {
+      setQuery('');
+      setResults([]);
+      setActive(null);
+
       el.current.querySelector('input').focus({ preventScroll: true });
+    }
   }, [open]);
 
   const onChange = evt => {
@@ -37,20 +46,36 @@ export const SearchWidget = props => {
 
     if (value.length > 3)
       setResults(index.search(value));
+    else
+      setResults([]);
+  }
+
+  const onKeyDown = evt => {
+    const { key } = evt;
+    if (key === 'ArrowDown') {
+      setActive(active === null ? 0 : Math.min(active + 1, results.length - 1));
+    } else if (key === 'ArrowUp' && active !== null) {
+      setActive(Math.max(0, active - 1));
+      console.log('up');
+    } else if (key === 'Enter' && active !== null) {
+      props.onSelectResult(results[active].item.annotation);
+    }
   }
 
   return (
     <div 
       ref={el}
+      onKeyDown={onKeyDown}
       className={open ? 'mrm-search-widget-wrapper open' : 'mrm-search-widget-wrapper'}>
 
       <input value={query} onChange={onChange} />
 
       {results.length > 0 && (
         <ul>
-          {results.map(result => (
+          {results.map((result, idx) => (
             <SearchResult 
               key={result.item.annotation.id}
+              active={idx === active}
               annotation={result.item.annotation} 
               matches={result.matches} 
               onSelect={props.onSelectResult} />
